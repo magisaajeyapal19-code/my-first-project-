c771e44a7fcfef6e893247520c09c9d2eeca228f
magisaajeyapal19-code <magisaajeyapal19@gmail.com>
2026-06-25 12:04:00 +0530

    Add files via upload

---
+++ sum multiples.py
@@ -0,0 +1,12 @@
+def sumOfMultiples(self, n):
+        sum=0
+        for i in range(1,n+1):
+            if i%3==0:
+                sum+=i
+            elif i%5==0:
+                sum+=i
+            elif i%7==0:
+                sum+=i
+       
+       
+        return sum
