b6e0a03a4a1c2df905cfc52f791cf9eb688c5b7c
MAGISAA J <magisaajeyapal19@gmail.com>
2026-07-03 12:13:32 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,10 @@
+class circle:
+     pi=3.14
+     radius=5
+     def area(self):
+          return circle.pi*(circle.radius**2)
+     def circumference(self):
+          return 2*circle.pi*circle.radius
+S1=circle()
+S1.area()
+S1.circumference()
